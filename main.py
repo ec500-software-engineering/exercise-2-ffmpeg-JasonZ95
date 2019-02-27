1adf63a67f71452605b06090c57440cd3054f16c
JasonZ95 <43214200+JasonZ95@users.noreply.github.com>
2019-02-26 22:53:41 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import subprocess
+import multiprocessing
+import os
+
+input_path = "C:/Users/zhoub/OneDrive/Desktop/ffmpeg"
+output_path = "C:/Users/zhoub/OneDrive/Desktop/ffmpeg"
+
+
+def ffmpeg_720(video_name):
+    bash_command = "ffmpeg -i " + input_path + "/" + video_name + \
+                   ' -r 30 -b 2M -s hd720 ' + output_path + "/" + video_name[:-4] + "_720.mp4"
+    subprocess.call(bash_command)
+    print("720-size video conversion finished")
+
+
+def ffmpeg_480(video_name):
+    bash_command = "ffmpeg -i " + input_path + "/" + video_name + \
+                   ' -r 30 -b 1M -s hd480 ' + output_path + "/" + video_name[:-4] + "_480.mp4"
+    subprocess.call(bash_command)
+    print("480-size video conversion finished")
+
+
+def run():
+    p = multiprocessing.Pool(processes=multiprocessing.cpu_count()-2)
+    print("The current computer has " + str(multiprocessing.cpu_count()) + " cpu cores.")
+    for video in os.listdir(input_path):
+        if video.endswith(".mp4"):
+            p.apply_async(ffmpeg_720, args=(video,))
+            p.apply_async(ffmpeg_480, args=(video,))
+    print("All processes start")
+    p.close()
+    p.join()
+
+
+def main():
+    run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,43 @@
+from pytest import approx
+import main
+import json
+import subprocess
+
+
+def ffprobe(file_name) -> dict:
+    """ get media metadata """
+
+    meta = subprocess.check_output(['ffprobe', '-v', 'warning',
+                                    '-print_format', 'json',
+                                    '-show_streams',
+                                    '-show_format',
+                                    file_name])
+    return json.loads(meta)
+
+
+def test():
+    fnin = 'test.mp4'
+    fnout_720 = 'test_720.mp4'
+    fnout_480 = 'test_480.mp4'
+    orig_meta = ffprobe(fnin)
+    orig_duration = float(orig_meta['streams'][0]['duration'])
+
+    main.main()
+
+    meta_720 = ffprobe(fnout_720)
+    duration_720 = float(meta_720['streams'][0]['duration'])
+
+    meta_480 = ffprobe(fnout_480)
+    duration_480 = float(meta_480['streams'][0]['duration'])
+
+    try:
+        assert orig_duration == approx(duration_720)
+        assert orig_duration == approx(duration_480)
+    except:
+        print("test not passed")
+
+    print("test passed")
+
+
+if __name__ == "__main__":
+    test()
